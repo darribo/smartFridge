6ffdb42e3303dd45a175caf032b37177f8db45e3
Daniel Rivera Bonilla <daniel.rivera.bonilla@udc.es>
2026-01-26 11:03:20 +0100

    Estructura del backend creada

---
+++ backend/smartfridge/src/test/java/es/udc/bonilla/rivera/daniel/smartfridge/SmartfridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.udc.bonilla.rivera.daniel.smartfridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartfridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
